7364ab37d94eff1e4d0b0a551fee49a80533c460
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-04-27 20:25:20 +0530

    added mangodb

---
+++ review-service/src/main/java/com/sitamahalakshmi/review_service/entity/ReviewDocument.java
@@ -0,0 +1,35 @@
+package com.sitamahalakshmi.review_service.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
+import java.util.UUID;
+import java.time.Instant;
+
+@Document(collection = "reviews")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ReviewDocument {
+
+    @Id
+    private String id; // MongoDB's internal ID (usually a String or ObjectId)
+
+    @Indexed // Indexing this makes searching for reviews by product very fast
+    private UUID productId;
+
+    @Indexed
+    private UUID userId;
+
+    private String userFullName;
+
+    private int rating;
+    private String comment;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+}

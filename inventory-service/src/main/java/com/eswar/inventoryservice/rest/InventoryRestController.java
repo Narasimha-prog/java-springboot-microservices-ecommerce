ddabfb96e6fec483c2fd2d7519aa440c2dd0e7b6
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-07-20 20:55:07 +0530

    added tag and operation for inventory

--- inventory-service/src/main/java/com/eswar/inventoryservice/rest/InventoryRestController.java
+++ inventory-service/src/main/java/com/eswar/inventoryservice/rest/InventoryRestController.java
@@ -3,6 +3,8 @@ package com.eswar.inventoryservice.rest;
 import com.eswar.inventoryservice.dto.InventoryDto;
 import com.eswar.inventoryservice.dto.PageResponse;
 import com.eswar.inventoryservice.service.IInventoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
@@ -16,29 +18,35 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/inventory")
+@Tag(name = "Inventory", description = "API for managing inventoryService in the e-commerce platform")
 public class InventoryRestController {
 
     private final IInventoryService inventoryService;
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "To add Stock details",  description = "login entry for Admin")
+
     public ResponseEntity<InventoryDto> addInventory(@RequestBody InventoryDto dto) {
         return
                 ResponseEntity.created(URI.create("/api/v1/inventory")).body(inventoryService.createInventory(dto));
     }
 
     @GetMapping("/{productId}")
+    @Operation(summary = "To get Stock details",  description = "login entry for user/admin")
     public ResponseEntity<InventoryDto> getInventory(@PathVariable UUID productId) {
         return ResponseEntity.ok(inventoryService.getInventory(productId));
     }
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "To get all Stock details",  description = "login entry for Admin")
     public ResponseEntity<PageResponse<InventoryDto>> getAllInventory( @ParameterObject Pageable pageable){
         return ResponseEntity.ok(inventoryService.getAllInventories(pageable));
     }
     @PutMapping("/{productId}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "To update Stock details",  description = "login entry for Admin")
     public ResponseEntity<InventoryDto> updateInventory(
             @PathVariable UUID productId,
             @RequestBody InventoryDto dto) {
--- user-service/src/test/java/com/eswar/userservice/UserIntegrationTest.java
+++ user-service/src/test/java/com/eswar/userservice/UserIntegrationTest.java
@@ -24,7 +24,6 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 

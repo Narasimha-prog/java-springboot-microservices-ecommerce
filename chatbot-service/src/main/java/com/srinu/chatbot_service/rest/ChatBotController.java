854935850c6887729d60bc090a8366ad5445e9eb
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-08-10 23:21:44 +0530

    added swgger methods for meansing full summary

--- chatbot-service/src/main/java/com/srinu/chatbot_service/rest/ChatBotController.java
+++ chatbot-service/src/main/java/com/srinu/chatbot_service/rest/ChatBotController.java
@@ -3,6 +3,8 @@ package com.srinu.chatbot_service.rest;
 import com.srinu.chatbot_service.dto.ChatRequest;
 import com.srinu.chatbot_service.dto.ChatResponse;
 import com.srinu.chatbot_service.service.IChatBotService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/chatbot")
 @RequiredArgsConstructor
+@Tag(name = "chatbot", description = "API for managing chatbot service in the e-commerce platform")
 public class ChatBotController {
 
     private final IChatBotService chatBotService;
 
     @PostMapping("/query")
+    @Operation(summary = "to ask queries,through post request ..",description = "to ask queries")
     public ResponseEntity<ChatResponse> handleUserQuery(@RequestBody ChatRequest request) {
         String aiAnswer = chatBotService.chatWithKnowledgeBase(request.message());
         return ResponseEntity.ofNullable(new ChatResponse(aiAnswer));
